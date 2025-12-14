ec739630cd2231e20e674f04be85c1a2a8c0cff4
leetaan1902@gmail.com <leetaan1902@gmail.com>
2025-12-14 12:16:32 +0700

    f

---
+++ src/main/java/org/example/informationmanagement/InformationManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.informationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/informationmanagement/InformationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.informationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
